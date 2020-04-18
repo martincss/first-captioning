0f8dd60f491d51b1d500930a6619ac16c451bdb1
Martín Carusso <martincarusso@gmail.com>
2020-04-18 04:24:29 -0300

    added caption tokenizer script

---
+++ preprocess_tokenize_captions.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+
+# Find the maximum length of any caption in our dataset
+def calc_max_length(tensor):
+    return max(len(t) for t in tensor)
+
+
+def caption_features(train_captions, top_k):
+
+    # Choose the top 5000 words from the vocabulary
+    # top_k = 5000
+    global tokenizer
+    tokenizer = tf.keras.preprocessing.text.Tokenizer(num_words=top_k,
+                                                      oov_token="<unk>",
+                                                      filters='!"#$%&()*+.,-/:;=?@[\]^_`{|}~ ')
+    tokenizer.fit_on_texts(train_captions)
+    train_seqs = tokenizer.texts_to_sequences(train_captions)
+
+
+    tokenizer.word_index['<pad>'] = 0
+    tokenizer.index_word[0] = '<pad>'
+
+    # Create the tokenized vectors
+    train_seqs = tokenizer.texts_to_sequences(train_captions)
+
+    # Pad each vector to the max_length of the captions
+    # If you do not provide a max_length value, pad_sequences calculates it automatically
+    cap_vector = tf.keras.preprocessing.sequence.pad_sequences(train_seqs, padding='post')
+    #
+    # # Calculates the max_length, which is used to store the attention weights
+    # max_length = calc_max_length(train_seqs)
+
+
+    return cap_vector#, max_length
