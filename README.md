772fa1abe68826f3d9a8d05a552ddde211c50be9
Martín Carusso <martincarusso@gmail.com>
2020-05-23 04:24:24 -0300

    updated to download validation set and readme with info on coco dataset

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # first-captioning
 
 A project to play around and experiment with **image captioning**. Copied directly from https://www.tensorflow.org/tutorials/text/image_captioning, based on the architecture of Show, Attend & Tell https://arxiv.org/abs/1502.03044
+
+
+# About dataset
+
+The [COCO dataset](http://cocodataset.org) (2014) is a large-scale object detection, segmentation, and captioning dataset.
+We will only be focused on captioning.
+
+The training set contains 82783 images (13GB), each with at least 5 captions. The validation set contains 41K images (6GB).
+
+ 
--- download_data.py
+++ download_data.py
@@ -3,24 +3,44 @@ import tensorflow as tf
 
 # Download caption annotation files
 annotation_folder = '/annotations/'
-image_folder = '/train2014/'
+image_train_folder = '/train2014/'
+image_validation_folder '/validation2014/'
+
+captions_url = 'http://images.cocodataset.org/annotations/annotations_trainval2014.zip'
+train_url = 'http://images.cocodataset.org/zips/train2014.zip'
+valid_url = 'http://images.cocodataset.org/zips/val2014.zip'
 
 
+# Download annotation files
 if not os.path.exists(os.path.abspath('.') + annotation_folder):
-  annotation_zip = tf.keras.utils.get_file('captions.zip',
+
+    print('Downloading annotation files')
+
+    annotation_zip = tf.keras.utils.get_file('captions.zip',
                                           cache_subdir=os.path.abspath('.'),
-                                          origin = 'http://images.cocodataset.org/annotations/annotations_trainval2014.zip',
+                                          origin = captions_url,
                                           extract = True)
-  annotation_file = os.path.dirname(annotation_zip)+'/annotations/captions_train2014.json'
-  os.remove(annotation_zip)
+    os.remove(annotation_zip)
 
 # Download image files
-if not os.path.exists(os.path.abspath('.') + image_folder):
-  image_zip = tf.keras.utils.get_file('train2014.zip',
+if not os.path.exists(os.path.abspath('.') + image_train_folder):
+
+    print('Downloading image training files')
+
+    image_zip = tf.keras.utils.get_file('train2014.zip',
                                       cache_subdir=os.path.abspath('.'),
-                                      origin = 'http://images.cocodataset.org/zips/train2014.zip',
+                                      origin = train_url,
                                       extract = True)
-  PATH = os.path.dirname(image_zip) + image_folder
-  os.remove(image_zip)
-else:
-  PATH = os.path.abspath('.') + image_folder
+    os.remove(image_zip)
+
+
+if not os.path.exists(os.path.abspath('.') + image_validation_folder):
+
+    print('Downloading image validation files')
+
+    image_zip = tf.keras.utils.get_file('val2014.zip',
+                                      cache_subdir=os.path.abspath('.'),
+                                      origin = 'valid_url,
+                                      extract = True)
+
+    os.remove(image_zip)
