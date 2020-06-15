3e32844912a0d60b5df6e5bbe0bca365475a98d8
Martín Carusso <martincarusso@gmail.com>
2020-06-15 06:51:43 -0300

    added maxlen to tokenizer, using train value in validation

--- preprocess_tokenize_captions.py
+++ preprocess_tokenize_captions.py
@@ -19,13 +19,15 @@ def make_tokenizer(train_captions, top_k):
 
 
 
-def caption_features(train_captions, tokenizer):
+def caption_features(train_captions, tokenizer, maxlen=None):
 
     # Create the tokenized vectors
     train_seqs = tokenizer.texts_to_sequences(train_captions)
 
     # Pad each vector to the max_length of the captions
     # If you do not provide a max_length value, pad_sequences calculates it automatically
-    cap_vector = tf.keras.preprocessing.sequence.pad_sequences(train_seqs, padding='post')
+    cap_vector = tf.keras.preprocessing.sequence.pad_sequences(train_seqs,
+                                                               padding='post',
+                                                               maxlen=maxlen)
 
     return cap_vector
--- valid_data_preparation.py
+++ valid_data_preparation.py
@@ -2,7 +2,7 @@ from utils import enable_gpu_memory_growth
 from preprocess_encode_images import extract_cache_features
 from preprocess_tokenize_captions import caption_features
 from data_preparation import image_fnames_captions, create_dataset
-from train_data_preparation import tokenizer
+from train_data_preparation import tokenizer, train_max_length
 
 from params import UPDATE_CACHE, num_examples_val, top_k
 
@@ -23,7 +23,7 @@ img_paths = all_img_paths[:num_examples_val]
 if UPDATE_CACHE:
     extract_cache_features(img_paths, IMGS_FEATURES_CACHE_DIR_VAL)
 
-cap_vector = caption_features(val_captions, tokenizer)
+cap_vector = caption_features(val_captions, tokenizer, maxlen=train_max_length)
 
 img_paths_val, cap_val = img_paths, cap_vector
 
