e8e0e0826e2136016368e0de1d5b79120681ee34
Martín Carusso <martincarusso@gmail.com>
2020-08-03 13:33:13 -0300

    added coco path con config, switched to pathlib

--- config.py
+++ config.py
@@ -1,33 +1,35 @@
 import os
 import sys
+from pathlib import Path
 
 # Set working directory to script location
-abspath = os.path.abspath(__file__)
-working_directory = os.path.dirname(abspath)
-os.chdir(working_directory)
+script_dir = Path(__file__).resolve().parent
+os.chdir(script_dir)
+coco_dir = script_dir / 'COCO'
 
-IMGS_FEATURES_CACHE_DIR_TRAIN = working_directory + '/image_features_train/'
-IMGS_FEATURES_CACHE_DIR_VAL = working_directory + '/image_features_val/'
+
+IMGS_FEATURES_CACHE_DIR_TRAIN = coco_dir / 'image_features_train'
+IMGS_FEATURES_CACHE_DIR_VAL = coco_dir / 'image_features_val'
 
 # Training data preparation
-IMGS_PATH_TRAIN = working_directory + '/train2014/'
-ANNOTATION_FILE_TRAIN = working_directory + '/annotations/captions_train2014.json'
+IMGS_PATH_TRAIN = coco_dir / 'train2014'
+ANNOTATION_FILE_TRAIN = coco_dir / 'annotations/captions_train2014.json'
 
 # Validation data
-IMGS_PATH_VAL = working_directory + '/val2014/'
-ANNOTATION_FILE_VAL = working_directory + '/annotations/captions_val2014.json'
+IMGS_PATH_VAL = coco_dir / 'val2014'
+ANNOTATION_FILE_VAL = coco_dir / 'annotations/captions_val2014.json'
 
 # Data saved during training
-CHECKPOINT_PATH = working_directory + "/checkpoints/train"
-#MODELS_PATH = working_directory + '/saved_models/'
-#RESULTS_PATH = working_directory + '/search_results/'
-GRID_SEARCHS_PATH = working_directory + '/grid_searchs/'
+CHECKPOINT_PATH = coco_dir / 'checkpoints/train'
+#MODELS_PATH = coco_dir + '/saved_models/'
+#RESULTS_PATH = coco_dir + '/search_results/'
+GRID_SEARCHS_PATH = coco_dir / 'grid_searchs'
 
 if __name__ == '__main__':
-    
+
     dirs = [IMGS_FEATURES_CACHE_DIR_TRAIN, IMGS_FEATURES_CACHE_DIR_VAL,
             CHECKPOINT_PATH, GRID_SEARCHS_PATH]
 
     for directory in dirs:
-        if not os.path.exists(directory):
-            os.mkdir(directory)
+        if not directory.exists():
+            directory.mkdir(parents=True)
