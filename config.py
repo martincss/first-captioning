ec246c47f91a77da820083fa95b57f11c97f01e2
Martín Carusso <martincarusso@gmail.com>
2020-08-07 19:41:28 -0300

    adapted config directories for both coco & iu x-ray

--- config.py
+++ config.py
@@ -6,30 +6,56 @@ from pathlib import Path
 script_dir = Path(__file__).resolve().parent
 os.chdir(script_dir)
 coco_dir = script_dir / 'COCO'
+iux_dir = script_dir / 'IU X-ray'
 
+DATASET_NAME = 'IU X-ray'
 
-IMGS_FEATURES_CACHE_DIR_TRAIN = coco_dir / 'image_features_train'
-IMGS_FEATURES_CACHE_DIR_VAL = coco_dir / 'image_features_val'
+ALL_DIRECTORIES = {
 
-# Training data preparation
-IMGS_PATH_TRAIN = coco_dir / 'train2014'
-ANNOTATION_FILE_TRAIN = coco_dir / 'annotations/captions_train2014.json'
+    'COCO':{
 
-# Validation data
-IMGS_PATH_VAL = coco_dir / 'val2014'
-ANNOTATION_FILE_VAL = coco_dir / 'annotations/captions_val2014.json'
+        # Training data preparation
+        'IMAGES_TRAIN': coco_dir / 'train2014',
+        'ANNOTATIONS_TRAIN': coco_dir / 'annotations/captions_train2014.json',
 
-# Data saved during training
-CHECKPOINT_PATH = coco_dir / 'checkpoints/train'
-#MODELS_PATH = coco_dir + '/saved_models/'
-#RESULTS_PATH = coco_dir + '/search_results/'
-GRID_SEARCHS_PATH = coco_dir / 'grid_searchs'
+        # Validation data
+        'IMAGES_VAL': coco_dir / 'val2014',
+        'ANNOTATIONS_VAL': coco_dir / 'annotations/captions_val2014.json',
+
+        # Cached image features
+        'IMAGE_FEATURES_TRAIN': coco_dir / 'image_features_train',
+        'IMAGE_FEATURES_VAL': coco_dir / 'image_features_val',
+
+        # Data saved during training
+        # CHECKPOINT_PATH = coco_dir / 'checkpoints/train'
+        #MODELS_PATH = coco_dir + '/saved_models/'
+        #RESULTS_PATH = coco_dir + '/search_results/'
+        'GRID_SEARCHS': coco_dir / 'grid_searchs'
+        },
+
+    'IU X-ray':{
+
+        # Training data preparation
+        'IMAGES': iux_dir / 'images',
+        'ANNOTATIONS': iux_dir / 'image_caption_pairs.json',
+
+        # Cached image features
+        'IMAGE_FEATURES': iux_dir / 'image_features',
+
+        # Data saved during training
+        # CHECKPOINT_PATH = coco_dir / 'checkpoints/train'
+        #MODELS_PATH = coco_dir + '/saved_models/'
+        #RESULTS_PATH = coco_dir + '/search_results/'
+        'GRID_SEARCHS': iux_dir / 'grid_searchs'
+        }
+
+}
+
+DIRECTORIES = ALL_DIRECTORIES[DATASET_NAME]
+
 
 if __name__ == '__main__':
 
-    dirs = [IMGS_FEATURES_CACHE_DIR_TRAIN, IMGS_FEATURES_CACHE_DIR_VAL,
-            CHECKPOINT_PATH, GRID_SEARCHS_PATH]
-
-    for directory in dirs:
+    for directory in DIRECTORIES.values():
         if not directory.exists():
             directory.mkdir(parents=True)
