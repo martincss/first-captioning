8e18ebe9fa765bd8eb8b34036320356802db8d11
Martín Carusso <martincarusso@gmail.com>
2020-08-06 22:39:44 -0300

    added image-caption json generation for IU X-ray

--- report_parsing.py
+++ report_parsing.py
@@ -1,3 +1,4 @@
+import json
 from bs4 import BeautifulSoup
 
 
@@ -32,3 +33,26 @@ def get_report(fname):
         report_info['auto_tags'] = [tag.text for tag in report.find_all('automatic')]
 
     return report_info
+
+
+def generate_image_caption_pairs(base_dir, save_to_json=False):
+
+    images_dir = base_dir / 'images'
+    reports_dir = base_dir / 'reports'
+
+    img_caption_pairs = {}
+
+    for report_fname in reports_dir.glob('*.xml'):
+
+        data = get_report(report_fname)
+
+        for id in data['ids']:
+            img_path = images_dir.resolve() / (id + '.png')
+            img_caption_pairs[str(img_path)] = data['impression'] + ' ' + \
+                                               data['findings']
+
+    if save_to_json:
+        with (base_dir / 'image_caption_pairs.json').open(mode = 'w') as f:
+            json.dump(img_caption_pairs, f)
+
+    return img_caption_pairs
